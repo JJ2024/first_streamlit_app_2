f031496ec5ac473a5058f7b1289c1ce5f37189b5
John Jireh B. Garcia <67573779+JJ2024@users.noreply.github.com>
2022-11-07 17:53:16 +0800

    Update streamlit_app.py
    Added menu headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 
 streamlit.title('My Parents\' New Healthy Diner')
-  
+streamlit.header('Omega 3 & Blueberry Oatmeal')
+streamlit.header('Kale, Spinach & Rocket Smoothie')
+streamlit.header('Hard-Boiled Free-Range Egg')
