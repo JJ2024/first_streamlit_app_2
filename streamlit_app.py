036da36696c680250841e2c207b1097b5ea5c4e1
John Jireh B. Garcia <67573779+JJ2024@users.noreply.github.com>
2022-11-08 23:38:50 +0800

    Update streamlit_app.py
    Added:
    
    "my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_cur = my_cnx.cursor()
    my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
    my_data_row = my_cur.fetchone()
    streamlit.text("Hello from Snowflake:")
    streamlit.text(my_data_row)
    "

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,29 @@
 import streamlit
 import pandas
+import snowflake.connector
 
-streamlit.title('My Parents\' New Healthy Diner')
-streamlit.header('Omega 3 & Blueberry Oatmeal')
-streamlit.header('Kale, Spinach & Rocket Smoothie')
-streamlit.header('Hard-Boiled Free-Range Egg')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#streamlit.title('My Parents\' New Healthy Diner')
+#streamlit.header('Omega 3 & Blueberry Oatmeal')
+#streamlit.header('Kale, Spinach & Rocket Smoothie')
+#streamlit.header('Hard-Boiled Free-Range Egg')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Let's put a pick list here so they can pick the fruit they want to include
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 
 #Display the table on the page
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
+#streamlit.dataframe(fruits_to_show)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
