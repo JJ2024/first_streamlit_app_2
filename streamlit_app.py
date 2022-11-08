cfd9c905f8f47aae5dba2ba0bbc30c7644132aee
John Jireh B. Garcia <67573779+JJ2024@users.noreply.github.com>
2022-11-08 16:47:57 +0800

    Update streamlit_app.py
    added "streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')"

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title('My Parents\' New Healthy Diner')
 streamlit.header('Omega 3 & Blueberry Oatmeal')
 streamlit.header('Kale, Spinach & Rocket Smoothie')
 streamlit.header('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
